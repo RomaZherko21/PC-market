3b790bf39ede5183d7ac1f65344e0f1f079b403e
Roma Zherko <RomaZherko21@inbox.ru>
2020-10-12 10:04:30 +0300

    carousel comp devised

--- src/components/main/mainPage/carousel/Сarousel.jsx
+++ src/components/main/mainPage/carousel/Сarousel.jsx
@@ -1,6 +1,11 @@
 import React, { } from 'react'
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import s from './Сarousel.module.css';
+
 
 import first from '../../../../images/mainCarousel/first.jpg';
 import second from '../../../../images/mainCarousel/second.jpg';
@@ -9,55 +14,27 @@ import fourth from '../../../../images/mainCarousel/fourth.jpg';
 
 
 
-class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arr: [first, second, third,fourth ],
-      counter: 0,
-      interval: '',
-    }
-
-    this.myRef = React.createRef();
-  }
-
-  componentDidMount() {
+const Carousel =()=> {
   
+  let settings = {
+    dots: false,
+    arrow: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+};
 
-
-    this.setState({
-      interval: setInterval(() => {
-        
-        const node = this.myRef.current;
-        node.className = s.fade;
-        setTimeout(()=>{node.className = ''},4900)
-     
-        if (this.state.counter < this.state.arr.length - 1) {
-          this.setState({
-            counter: this.state.counter + 1,
-          })
-        } else {
-          this.setState({
-            counter: 0,
-          })
-        }
-      }, 5000)
-    })
-  }
-  componentWillUnmount() {
-    clearInterval(this.state.interval);
-    this.setState({
-      interval: '',
-    })
-  }
-
-  render() {
     return (
       <div className={s.carousel}>
-        <img src={this.state.arr[this.state.counter]} ref={this.myRef} alt="" />
+        <Slider {...settings}>
+        <img src={first} alt="" />
+        <img src={second} alt="" />
+        <img src={third} alt="" />
+        <img src={fourth} alt="" />
+      </Slider>
       </div>
     )
-  }
+  
 
 }
 
