f68c0d416f468bd94976f87fc62ab5184419a23a
Roma Zherko <RomaZherko21@inbox.ru>
2020-10-24 09:52:51 +0300

    show users API

--- src/components/main/showUsers/ShowUsers.jsx
+++ src/components/main/showUsers/ShowUsers.jsx
@@ -2,49 +2,25 @@ import React from 'react'
 import s from './ShowUsers.module.css'
 import user from '../../../images/user.svg'
 
-import * as axios from 'axios'
 
-
-class ShowUsers extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onPageChange = this.onPageChange.bind(this);
-  }
-
-  componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.state.count}&page=${this.props.state.page}`).then(response => {
-      this.props.addUsers(response.data.items)
-    })
-  }
-
-
-
-  onPageChange(page, num) {
-      this.props.onPageChange(num);
-      
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.state.count}&page=${page+num}`).then(response => {
-      this.props.addUsers(response.data.items)
-    })
-  }
-  render() {
-    return (
-      <section className={s.allUsers}>
-        {this.props.state.usersList.map(item => {
-          return (
-            <div className={s.oneUser}>
-              <img src={item.photos.small != null ? item.photos.small : user} alt="" />
-              {item.name}
-            </div>
-          )
-        })}
-        <div className={s.allUsersButtons}>
-          <button onClick={()=>this.onPageChange(this.props.state.page, -1)}>Previous</button>
-          <button onClick={()=>this.onPageChange(this.props.state.page, 1)}>Next</button>
-        </div>
-      </section>
-    );
-  }
+function ShowUsers(props){
+  console.log(props.state)
+  return (
+    <section className={s.allUsers}>
+      {props.state.usersList.map(item => {
+        return (
+          <div className={s.oneUser}>
+            <img src={item.photos.small != null ? item.photos.small : user} alt="" />
+            {item.name}
+          </div>
+        )
+      })}
+      <div className={s.allUsersButtons}>
+        <button onClick={() => props.state.page !== 1 && props.onPageChange(props.state.page, -1)}>Previous</button>
+        <button onClick={() => props.onPageChange(props.state.page, 1)}>Next</button>
+      </div>
+    </section>
+  );
 }
-
 
 export default ShowUsers;
--- src/components/main/showUsers/ShowUsersContainer.jsx
+++ src/components/main/showUsers/ShowUsersContainer.jsx
@@ -5,7 +5,34 @@ import { connect } from 'react-redux'
 import addUsersAC from './actionCreators/addUsersAC'
 import onPageChangeAC from './actionCreators/onPageChangeAC'
 
+import React from 'react'
+import * as axios from 'axios'
 
+
+class ShowUsersContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onPageChange = this.onPageChange.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.state.count}&page=${this.props.state.page}`).then(response => {
+      this.props.addUsers(response.data.items)
+    })
+  }
+  onPageChange(page, num) {
+    this.props.onPageChange(num);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.state.count}&page=${page + num}`).then(response => {
+      this.props.addUsers(response.data.items)
+    })
+  }
+  render() {
+    return (
+      <ShowUsers state={this.props.state}
+        onPageChange={this.onPageChange} />
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -19,6 +46,6 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const ShowUsersContainer = connect(mapStateToProps, mapDispatchToProps)(ShowUsers);
 
-export default ShowUsersContainer;
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowUsersContainer);
